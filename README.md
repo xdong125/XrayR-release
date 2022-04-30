77ea9e8b1353135691e50b43ec54c53f5b0221cb
css1122 <82438606+css1122@users.noreply.github.com>
2022-04-30 23:45:10 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# 注意
-原作者己删库，仅只是复制备份做成自己的仓库供自己使用！！！
 # XRayR
 A Xray backend framework that can easily support many panels.
 
 一个基于Xray的后端框架，支持V2ay,Trojan,Shadowsocks协议，极易扩展，支持多面板对接
 
-Find the source code here: [XrayR-project/XrayR](https://github.com/XrayR-project/XrayR)
+Find the source code here: [Misaka-blog/XrayR](https://github.com/Misaka-blog/XrayR)
 
 # 详细使用教程
 
@@ -14,26 +12,29 @@ Find the source code here: [XrayR-project/XrayR](https://github.com/XrayR-projec
 # 一键安装
 
 ```
-bash <(curl -Ls https://raw.githubusercontent.com/css1122/XrayR-release/main/install.sh)
+wget -N https://raw.githubusercontents.com/Misaka-blog/XrayR-script/master/install.sh && bash install.sh
 ```
+
 # Docker 安装
 
 ```
-docker pull crackair/xrayr:latest && docker run --restart=always --name xrayr -d -v ${PATH_TO_CONFIG}/config.yml:/etc/XrayR/config.yml --network=host crackair/xrayr:latest
+docker pull misakano7545/xrayr:latest && docker run --restart=always --name xrayr -d -v ${PATH_TO_CONFIG}/config.yml:/etc/XrayR/config.yml --network=host misakano7545/xrayr:latest
 ```
 
 # Docker compose 安装
-0. 安装docker-compose: 
+
+1. 安装docker-compose: 
 ```
 curl -fsSL https://get.docker.com | bash -s docker
 curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ```
-1. `git clone https://github.com/Misaka-blog/XrayR-script`
-2. `cd XrayR-script`
-3. 编辑config。
+
+2. `git clone https://github.com/Misaka-blog/XrayR-script XrayR-release`
+3. `cd XrayR-release`
+4. 编辑config。
 配置文件基本格式如下，Nodes下可以同时添加多个面板，多个节点配置信息，只需添加相同格式的Nodes item即可。
-4. 启动docker：`docker-compose up -d`
+5. 启动docker：`docker-compose up -d`
 ```
 Log:
   Level: none # Log level: none, error, warning, info, debug 
@@ -113,7 +114,9 @@ Nodes:
 ```
 
 ## Docker compose升级
+
 在docker-compose.yml目录下执行：
+
 ```
 docker-compose pull
 docker-compose up -d
--- install.sh
+++ install.sh
@@ -102,20 +102,20 @@ install_acme() {
 
 install_XrayR() {
     if [[ -e /usr/local/XrayR/ ]]; then
-        rm /usr/local/XrayR/ -rf
+        rm -rf /usr/local/XrayR/
     fi
 
     mkdir /usr/local/XrayR/ -p
-	cd /usr/local/XrayR/
+    cd /usr/local/XrayR/
 
     if  [ $# == 0 ] ;then
-        last_version=$(curl -Ls "https://api.github.com/repos/css1122/XrayR-release/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        last_version=$(curl -Ls "https://api.github.com/repos/Misaka-blog/XrayR/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
             echo -e "${red}检测 XrayR 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 XrayR 版本安装${plain}"
             exit 1
         fi
         echo -e "检测到 XrayR 最新版本：${last_version}，开始安装"
-        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/css1122/XrayR-release/releases/download/${last_version}/XrayR-linux-${arch}.zip
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/Misaka-blog/XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 XrayR 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
@@ -174,13 +174,15 @@ install_XrayR() {
     if [[ ! -f /etc/XrayR/custom_inbound.json ]]; then
         cp custom_inbound.json /etc/XrayR/
     fi
-    if [[ ! -f /etc/XrayR/ruelist ]]; then
-        cp ruelist /etc/XrayR/
+    if [[ ! -f /etc/XrayR/rulelist ]]; then
+        cp rulelist /etc/XrayR/
     fi
     curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/Misaka-blog/XrayR-script/master/XrayR.sh
     chmod +x /usr/bin/XrayR
     ln -s /usr/bin/XrayR /usr/bin/xrayr # 小写兼容
     chmod +x /usr/bin/xrayr
+    cd $cur_dir
+    rm -f install.sh
     echo -e ""
     echo "XrayR 管理脚本使用方法 (兼容使用xrayr执行，大小写不敏感): "
     echo "------------------------------------------"
